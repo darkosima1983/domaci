ac9c16b30a8559cdb94c3fb600248ce476d4bb9d
darkosima1983 <darkosima1983@gmail.com>
2025-02-16 19:30:03 +0100

    Add files via upload

--- domaci.php
+++ domaci.php
@@ -1,7 +1,26 @@
 <?php
 $title = "Postani Programer";
 $naslov = "Postani Programer";
-$nav =["Glavna", "O nama", "Kontakt"];
+$footer = date("Y");
+$nav =[
+        [
+            "naziv"=>"Glavna", 
+            "link"=>"home.php",
+        ],
+
+        [
+            "naziv"=>"O nama",
+            "link"=>"about_us.php",
+        ], 
+        [ 
+            "naziv"=>"Kontakt",
+            "link"=>"contact.php",
+
+        ],
+
+
+
+    ];
 
 
 ?>
@@ -15,10 +34,11 @@ $nav =["Glavna", "O nama", "Kontakt"];
 <body>
     <h1><?= $naslov ?></h1>
     <nav>
-        <a href="#"><?= $nav[0] ?></a>
-        <a href="#"><?= $nav[1] ?></a>
-        <a href="#"><?= $nav[2] ?></a>
+        <a href=<?="{$nav[0]['link'] }"?>><?= $nav[0]['naziv'] ?></a>
+        <a href=<?="{$nav[1]['link'] }"?>><?= $nav[1]['naziv'] ?></a>
+        <a href=<?="{$nav[2]['link'] }"?>><?= $nav[2]['naziv'] ?></a>
 
     </nav>
 </body>
+<footer>Copyroght © mojsajt <?=$footer?></footer>
 </html>
